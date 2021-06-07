f4b023bbd961c206bc6fc428a1d470ff3b321e89
SwiftGoose5 <swiftgoose19@gmail.com>
2021-06-07 19:20:03 -0400

    First commit

---
+++ PullTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PullTest
+//
+//  Created by Goose on 6/7/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
