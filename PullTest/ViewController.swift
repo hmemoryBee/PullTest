e9a2714deb0780c2d31cf5d84048da57ddda12cf
SwiftGoose5 <swiftgoose19@gmail.com>
2021-06-08 14:04:21 -0400

    Deleted 1 quote

--- PullTest/ViewController.swift
+++ PullTest/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("Wake up Mr. Freeman. Wake up and smell the ashes")
+        
         print("Time to choose Mr Freeman")
         print("It's time to choose")
         
